fcd12c6f3f16b21e6abb8e63c4410205406140c9
Zach <zrauch@purdue.edu>
2021-02-09 17:18:39 -0500

    adding stupid comments

--- compressible.py
+++ compressible.py
@@ -15,7 +15,7 @@ gamma = 1.4
 R_SI = 287 # J/kg-K
 
 
-def D(M,gamma):
+def D(M,gamma): # mass flow function, see Mattingly and Boyer
 	M_2 = pow(M,2)
 	isen = (1 + ((gamma-1)/2)*M_2)
 	power = (gamma+1)/(2*(gamma-1))
@@ -27,14 +27,14 @@ def D(M,gamma):
 		dD_dM = (D/M)*(1-M_2)/isen
 	return D,dD_dM
 
-def G(M,gamma):
+def G(M,gamma): # thrust/momentum flow function, see Mattingly and Boyer
 	M_2 = pow(M,2)
 	isen = (1 + ((gamma-1)/2)*M_2)
 	G = (1+gamma*M_2)/pow(isen,(gamma/(gamma-1)))
 	dG_dM = gamma*G*M*(2/(1+gamma*M_2) - 1/(isen))
 	return G, dG_dM
 
-def N(M,gamma):
+def N(M,gamma): # the no-named function?? see Mattingly and Boyer
 	M_2 = pow(M,2)
 	isen = (1 + ((gamma-1)/2)*M_2)
 	N = D(M,gamma)[0]/G(M,gamma)[0]
@@ -44,7 +44,7 @@ def N(M,gamma):
 		dN_dM = N*(1/M + ((gamma-1)/2)*(M/isen) - (2*gamma*M/(1+gamma*M_2)))
 	return N, dN_dM
 
-def areaMachRelation(M_value,A_ratio,M_init):
+def areaMachRelation(M_value,A_ratio,M_init): # derived from mass flow using isentropic relations, see Mattingly&Boyer or Zucrow (1976)
 	DM = D(M_value,gamma)[0]
 	M_guess = M_init
 	eps = 1 # initialize with eps > delta
@@ -55,7 +55,7 @@ def areaMachRelation(M_value,A_ratio,M_init):
 		M_guess = M_new
 	return M_new
 
-def newtonRaphsonMach(M1,A_ratio):
+def newtonRaphsonMach(M1,A_ratio): # newton-raphson solver for M2 given M1 and area ratio of a nozzle
 	eps = 1
 	D1,unused = D(M1,gamma)
 	M_guess = 0.5
@@ -66,7 +66,7 @@ def newtonRaphsonMach(M1,A_ratio):
 		M_guess = M2
 	return M_guess
 
-def newtonRaphsonMachSHOCK(N1,gamma): 
+def newtonRaphsonMachSHOCK(N1,gamma): # newton-raphson solver for M2 after a shock, given N(M1)
 	eps = 1
 	M_guess = 0.75
 	while eps > 1e-4:
@@ -76,7 +76,7 @@ def newtonRaphsonMachSHOCK(N1,gamma):
 		M_guess = M2
 	return M_guess
 
-def newtonRaphsonSupersonic(M1,gamma):
+def newtonRaphsonSupersonic(M1,gamma): # newton-raphson solver for a supersonic M2 given a subsonic M1
 	eps = 1
 	D1,unused = D(M1,gamma)
 	M_guess = 2.5
@@ -87,18 +87,7 @@ def newtonRaphsonSupersonic(M1,gamma):
 		M_guess = M2
 	return M_guess
 
-def areaMachRelation(M_value,A_ratio,M_init):
-	DM = D(M_value,gamma)[0]
-	M_guess = M_init
-	eps = 1 # initialize with eps > delta
-	while (eps > 1e-6):
-		D_guess,dD_dM_guess = D(M_guess,gamma)
-		M_new = M_guess + (A_ratio*DM-D_guess)/dD_dM_guess
-		eps = np.abs(M_new-M_guess)
-		M_guess = M_new
-	return M_new
-
-def isentropicRelationCalculator(M,gamma,printout):
+def isentropicRelationCalculator(M,gamma,printout): # isentropic relations calculator, does not currently include sonic relations
 	isen = 1 + (gamma-1)/2*M**2
 	T0_T = isen
 	p0_p = isen**(gamma/(gamma-1))
@@ -114,7 +103,7 @@ def isentropicRelationCalculator(M,gamma,printout):
 		print("\n")
 	return [T0_T, p0_p, rho0_rho]
 
-def normalShockCalculator(M,gamma,printout):
+def normalShockCalculator(M,gamma,printout): # normal shock calculator, equations derived in Mattingly and Boyer
 	M2 = np.sqrt(((gamma-1)*M**2 + 2)/(2*gamma*M**2 - (gamma-1)))
 	p02_p01 = ( ((gamma+1)*M**2)/((gamma-1)*M**2 + 2) )**(gamma/(gamma-1)) * ( (gamma+1)/(2*gamma*M**2-(gamma-1)) )**(1/(gamma-1))
 	T2_T1 = (2*gamma*M**2-(gamma-1))*((gamma-1)*M**2+2)/((gamma+1)**2*M**2)
