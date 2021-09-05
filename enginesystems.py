c53dea36d4e71b5ee9ebfd52d3acf0eef3c1a6cb
Zach <zrauch@purdue.edu>
2021-09-05 19:07:21 -0400

    updates

--- enginesystems.py
+++ enginesystems.py
@@ -8,49 +8,8 @@ import sys,os
 from scipy.optimize import fsolve
 import spatial_discretization
 
-## variables
-global gamma, R, Cp, R_SI,CP_SI,R_ENG,CP_ENG
-R_SI = 287 # J/kg-K -- m^2/s^2-K\
-CP_SI = 1004.5 # J/kg-K
-R_ENG = 53.353 # ft-lbf/lbm-R -- needs multiplied by g for most applications
-#R_ENG = 1717 # ft^2/s^2-R
-CP_ENG = 0.3 # Btu/lbm-R
-
-global g,convertBTU,StoHR,IN2toFT2,FtoR
-g = 32.174 # [ft-lbm/lbf-s2]
-convertBTU = 778.17 # [ft-lbf/BTU]
-StoHR = 3600 # [s/hr]
-IN2toFT2 = 144 # [144 in2/ft2]
-FtoR = 459.67
-
-inputfound = 0; count = 0; limit = 4
-unitsys = input("Are you working in the Metric (M) or Imperial (I) unit system?\t")
-options = ["Metric","metric","M","English","english","E","Imperial","imperial","I","Ass","ass"]
-if (unitsys in options):
-	inputfound = 1
-
-while (inputfound != 1):
-	if (count == 0):
-		print(unitsys,"is not a valid input, please enter one of the following:")
-		print(options)
-	unitsys = input("Are you working in the Metric (M) or Imperial (I) unit system?\t")
-	count+=1
-	if (unitsys in options):
-		inputfound = 1
-	elif (count == limit):
-		print("\nHEY! \nstop being a dumbass and enter one of the values above. Last chance.")
-	elif (count > limit):
-		print("\nget outta here you filthy animal")
-		exit(0)
-
-if (options.index(unitsys) < 3):
-	R = R_SI
-	Cp = CP_SI
-else:
-	R = R_ENG
-	Cp = CP_ENG
-
-gamma = 1.4
+# setunits defines unit system-dependent things like R, g, and Cp
+from setunits import *
 
 ################################################################################################### 
 # enginesystems.py is a library intended to contain all relevant python codes developed for the
@@ -60,7 +19,16 @@ gamma = 1.4
 # most included routines are functions of M, stagnation properties, combustion product temperature,
 # and the necessary parameters for each unique system.
 ################################################################################################### 
-
+def milstd_5008_B(M):
+	if M<=1:
+		p02_p0 = 1
+	elif M>1 and M<5:
+		p02_p0 = 1 - 0.075*(M-1)**1.35
+	elif M>=5:
+		p02_p0 = 800/(M**4 + 935)
+	else:
+		print("wtf r u doing bro, M can't be negative")
+	return p02_p0
 
 # turbofan :: a function to solve the complete IDEAL turbofan engine system.
 # Note that the equations derived for use in this function depend on some critical assumptions,
@@ -78,38 +46,27 @@ gamma = 1.4
 # 2. specific fuel consumption, SFC [check these units]
 def turbofan(M, p, T, pi_c, pi_f, B, T04, delta_HB):
 	## IDEAL Turbofan Calculations
-	a0 = np.sqrt(gamma*R*g*T)
-	u0 = M*a0
+	u0 = M*np.sqrt(gamma*R*T)      
 	# 0 --> 2 : compression through ideal diffuser
-	if M<1:
-		p02 = p*(1+((gamma-1)/2)*M**2)**(gamma/(gamma-1))
-	elif M>=1 and M<3:
-		p02 = p*(1 - 0.075*(M-1)**1.35)
-	
+	p0 = p*(1+((gamma-1)/2)*M**2)**(gamma/(gamma-1))
 	T02 = T00 = T*(1+((gamma-1)/2)*M**2)
-	tau_r = T00/T
+	p02 = p0*milstd_5008_B(M)
 
 	# 2 --> 3: compression through ideal compressor
 	p03 = pi_c*p02
 	T03 = T02*(pi_c)**((gamma-1)/gamma)
-	tau_c = T03/T02
 
 	# 2 --> 3' : compression through fan
 	p03_p = pi_f*p02
 	T03_p = T02*(pi_f)**((gamma-1)/gamma)
-	tau_F = T03_p/T02
 
 	# 3 --> 4: isobaric combustion process
-	tau_b = T04/T03
-	tau_lamba = tau_r*tau_c*tau_b
 	p04 = p03
 	f = Cp*(T04-T03)/(delta_HB-Cp*T04)
-	#f = Cp*T00/delta_HB * (tau_lamba - tau_r*tau_c)
 
 	# 4 --> 5: expansion through ideal turbine
-	T05 = T04*(1 - (T02/T04)*((tau_c-1) + B*(tau_F-1)))
-	tau_t = T05/T04
-	p05 = p04*tau_t**((gamma-1)/gamma)
+	T05 = T04*(1 - (T02/T04)*((T03/T02-1) + B*(T03_p/T02-1)))
+	p05 = p04*(T05/T04)**((gamma-1)/gamma)
 
 	# 5 -- 9: expansion through ideal nozzle
 	p09 = p05
@@ -119,15 +76,18 @@ def turbofan(M, p, T, pi_c, pi_f, B, T04, delta_HB):
 	p09_p = p03_p
 	T09_p = T03_p
 
-	## Turbofan
+	u9 = np.sqrt(convertBTU*g* 2*Cp*T09*(1 - (p/p09)**((gamma-1)/gamma) ))
+	u9_p = np.sqrt(convertBTU*g* 2*Cp*T03_p*(1 - (p/p03_p)**((gamma-1)/gamma)))
+
+	tau_b = T04/T03; tau_r = T00/T; tau_c = T03/T02; tau_t = T05/T04; tau_F = T03_p/T02
 	u9 = u0*np.sqrt(tau_b*(tau_r*tau_c*tau_t - 1)/(tau_r-1))
 	M9_p = np.sqrt((2/(gamma-1))*(tau_r*tau_F - 1))
 	T9_p = T09_p/(1+(gamma-1)/2*M9_p**2)
-	a9 = np.sqrt(gamma*R*g*T9_p)
+	a9 = np.sqrt(gamma*R*T9_p)
 	u9_p = M9_p*a9
 
-	ST = ((a0*M/(1+B))*((u9/u0 - 1) + B*(u9_p/u0 - 1)))
-	SFC = f/((1+B)*ST)*g#*StoHR
+	ST = ( u0/(1+B) * ((u9/u0 - 1) + B*(u9_p/u0 - 1)) )/g
+	SFC = f/((1+B)*ST)#*StoHR
 	return [ST, SFC]
 
 # turbojet :: a function to solve the complete IDEAL turbojet engine system.
@@ -142,13 +102,14 @@ def turbofan(M, p, T, pi_c, pi_f, B, T04, delta_HB):
 # outputs:
 # 1. specific thrust, ST [s]
 # 2. specific fuel consumption, SFC [check these units]
-def turbojet(M,p,T,pi_c,delta_HB):
+def turbojet(M, p, T, pi_c, T04, delta_HB):
 	## IDEAL Turbofan Calculations
-	a0 = np.sqrt(gamma*R*g*T)
+	a0 = np.sqrt(gamma*R*T)
 	u0 = M*a0
 	# 0 --> 2 : compression through ideal diffuser
-	p02 = p00 = p*(1+((gamma-1)/2)*M**2)**(gamma/(gamma-1))
+	p0 = p*(1+((gamma-1)/2)*M**2)**(gamma/(gamma-1))
 	T02 = T00 = T*(1+((gamma-1)/2)*M**2)
+	p02 = p0*milstd_5008_B(M)
 	tau_r = T00/T
 
 	# 2 --> 3: compression through ideal compressor
@@ -164,7 +125,7 @@ def turbojet(M,p,T,pi_c,delta_HB):
 	#f = Cp*T00/delta_HB * (tau_lamba - tau_r*tau_c)
 
 	# 4 --> 5: expansion through ideal turbine
-	T05 = T04*(1 - (T02/T04)*(tau_c-1))
+	T05 = T04*(1 - (T02/T04)*(tau_c-1))/(1+f)
 	tau_t = T05/T04
 	p05 = p04*tau_t**((gamma-1)/gamma)
 
@@ -172,23 +133,20 @@ def turbojet(M,p,T,pi_c,delta_HB):
 	T09 = T05
 	p09 = p05
 
-	## Ideal Afterburning Turbojet
-	#f_total = f+f_ab
-	M9 = np.sqrt(2/(gamma-1)*(tau_r*tau_c*tau_t-1))
-	T9 = T09/(1+(gamma-1)/2*M9**2)
-	a9 = np.sqrt(gamma*R*T9*g)
-	u9 = M9*a9
+	## Ideal Turbojet
+	u9 = np.sqrt(convertBTU*g* 2*Cp*T09*(1 - (p/p09)**((gamma-1)/gamma)))
 	ST = a0*M*((1+f)*u9/u0 -1)/g
-	SFC = f/ST*StoHR
+	SFC = f/ST#*StoHR
 	return [ST, SFC]
 
-def afterburningTurbojet(M,p,T,pi_c,T07):
+def afterburningTurbojet(M, p, T, pi_c, T04, T07, delta_HB):
 	## IDEAL Turbofan Calculations
-	a0 = np.sqrt(gamma*R*g*T)
+	a0 = np.sqrt(gamma*R*T)
 	u0 = M*a0
 	# 0 --> 2 : compression through ideal diffuser
-	p02 = p00 = p*(1+((gamma-1)/2)*M**2)**(gamma/(gamma-1))
+	p0 = p*(1+((gamma-1)/2)*M**2)**(gamma/(gamma-1))
 	T02 = T00 = T*(1+((gamma-1)/2)*M**2)
+	p02 = p0*milstd_5008_B(M)
 	tau_r = T00/T
 
 	# 2 --> 3: compression through ideal compressor
@@ -225,10 +183,10 @@ def afterburningTurbojet(M,p,T,pi_c,T07):
 	#f_total = f+f_ab
 	M9 = np.sqrt(2/(gamma-1)*(tau_r*tau_c*tau_t-1))
 	T9 = T09/(1+(gamma-1)/2*M9**2)
-	a9 = np.sqrt(gamma*R*T9*g)
+	a9 = np.sqrt(gamma*R*T9)
 	u9 = M9*a9
 	ST = a0*M*((1+f_total)*u9/u0 -1)/g
-	SFC = f_total/ST*StoHR
+	SFC = f_total/ST
 	return [ST, SFC]
 
 def afterburningTurbojetWithBleeds(M,p,T,tpr,T07,eps1,eps2,eps3,p0ratio):
@@ -282,12 +240,13 @@ def afterburningTurbojetWithBleeds(M,p,T,tpr,T07,eps1,eps2,eps3,p0ratio):
 
 	return [ST,SFC,T0_list,p0_list,M9,f_total,f_ab]
 
-def ramjet(M,p,T):
-	a0 = np.sqrt(gamma*R*g*T)
+def ramjet(M, p, T, T04, delta_HB):
+	a0 = np.sqrt(gamma*R*T)
 	u0 = M*a0
 	# 0 --> 2 : compression through ideal diffuser
-	p02 = p00 = p*(1+((gamma-1)/2)*M**2)**(gamma/(gamma-1))
+	p0 = p*(1+((gamma-1)/2)*M**2)**(gamma/(gamma-1))
 	T02 = T00 = T*(1+((gamma-1)/2)*M**2)
+	p02 = p0*milstd_5008_B(M)
 	tau_r = T00/T
 
 	# 2 --> 3: compression through ideal compressor
@@ -311,18 +270,17 @@ def ramjet(M,p,T):
 	T09 = T05
 	p09 = p05
 
-	#f_total = f+f_ab
 	M9 = np.sqrt(2/(gamma-1)*(tau_r*tau_c*tau_t-1))
 	T9 = T09/(1+(gamma-1)/2*M9**2)
-	a9 = np.sqrt(gamma*R*T9*g)
+	a9 = np.sqrt(gamma*R*T9)
 	u9 = M9*a9
 	ST = a0*M*((1+f)*u9/u0 -1)/g
-	SFC = f/ST*StoHR
+	SFC = f/ST
 	return [ST, SFC]
 
 def nozzleExitArea(M9,T09,p09,mdot):
 	T9 = T09/(1+(gamma-1)/2*M9**2)
-	a9 = np.sqrt(gamma*R*g*T9)
+	a9 = np.sqrt(gamma*R*T9)
 	u9 = a9*M9
 	rho9 = p/(R*g*T9)
 	A9 = mdot/(rho9*u9)/g # calculate nozzle exit area based on M9
@@ -330,7 +288,7 @@ def nozzleExitArea(M9,T09,p09,mdot):
 
 def nozzleThroatArea(M8,T08,p08,mdot):
 	T8 = T08/(1+(gamma-1)/2*M8**2)
-	a8 = np.sqrt(gamma*R*g*T8)
+	a8 = np.sqrt(gamma*R*T8)
 	u8 = a8*M8
 	p8 = p08/(1+(gamma-1)/2*M8**2)**(gamma/(gamma-1))
 	rho8 = p8/(R*g*T8)
